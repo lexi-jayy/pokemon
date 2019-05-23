75b18a8788c1920bd61e36284c3fcaee9006ca72
Lexi-Jayy Critchley <845869864@bun2-206-18.local>
2019-05-23 15:25:08 +1200

    saved custom fields

--- inc/custom_fields.php
+++ inc/custom_fields.php
@@ -13,25 +13,45 @@ $metaboxes = array(
             'price' => array(
                 'title' => 'Post Price',
                 'type' => 'number',
-                'description' => 'The Price of the Post' 
+                'description' => 'The price of the post'
             ),
-            'select' => array(
-                'title' => 'Select Choice',
+            'side' => array(
+                'title' => 'What side is it on?',
                 'type' => 'select',
-                'description' => 'select',
-                'choices' => array(
-
-                )
+                'description' => '',
+                'choices' => array('Left', 'Right')
+            ),
+            'extra_content' => array(
+                'title' => 'Extra Content',
+                'type' => 'textarea',
+                'description' => '',
+                'rows' => 5
             )
         )
     ),
     'page_meta' => array(
         'title' => 'Extra Page Information',
-        'post_type' => 'page'
-    ),
-    'event_meta' => array(
-        'title' => 'Extra event Information',
-        'post_type' => 'event'
+        'post_type' => 'page',
+        'fields' => array(
+            'width' => array(
+                'title' => 'width',
+                'type' => 'select',
+                'description' => 'What is the width that you would like?',
+                'choices' => array('Full Width', 'Half Width')
+            ),
+        ),
+        'events_meta' => array(
+            'title' => 'Extra Event Information',
+            'post_type' => 'event',
+            'fields' => array(
+                'Event' => array(
+                    'title' => 'Events',
+                    'type' => 'select',
+                    'description' => 'Choose An Event?',
+                    'choices' => array('Event 1', 'Wvent 2')
+                ),
+        ),
+    )
     )
 );
 
@@ -51,39 +71,94 @@ function create_custom_meta_boxes() {
 add_action('admin_init', 'create_custom_meta_boxes');
 
 
-
+//function needs to be unique name
 function output_custom_meta_box($post, $metabox){
-    // var_dump($metabox);
-    // echo '<h1>'.$metabox['title'].'</h1>';
     $fields = $metabox['args']['fields'];
+
+    $customValues = get_post_custom($post->ID);
+
+    echo '<input type="hidden" name="post_format_meta_box_nonce" value="'.wp_create_nonce( basename(__FILE__) ).'">';
+
     if($fields){
         foreach ($fields as $fieldID => $field) {
             switch($field['type']){
                 case 'text':
-                    echo '<label>'.$field['title'].'</label>';
-                    echo '<input type="text" class="inputField">';
+                    echo '<label for="'.$fieldID.'">'.$field['title'].'</label>';
+                    echo '<input type="text" name="'.$fieldID.'" class="inputField" value="'.$customValues[$fieldID][0].'">';
                 break;
-
                 case 'number':
-                    echo '<label>'.$field['title'].'</label>';
-                    echo '<input type="number" class="inputField">';
+                    echo '<label for="'.$fieldID.'">'.$field['title'].'</label>';
+                    echo '<input type="number" name="'.$fieldID.'" class="inputField" value="'.$customValues[$fieldID][0].'">';
                 break;
-
-                case 'email':
-
+                case 'textarea':
+                    echo $customValues[$fieldID][0];
+                    echo '<br>';
+                    echo '<label for="'.$fieldID.'">'.$field['title'].'</label>';
+                    echo '<textarea class="inputField" name="'.$fieldID.'" rows="'.$field['rows'].'"></textarea>';
                 break;
-
                 case 'select':
-                    echo '<label>'.$field['title'].'</label>';
-                    echo '<input type="select"'. $fieldID . 'class="inputField">';
-
+                    echo $customValues[$fieldID][0];
+                    echo '<br>';
+                    echo '<label for="'.$fieldID.'">'.$field['title'].'</label>';
+                    echo '<select name="'.$fieldID.'" class="inputField customSelect">';
+                        echo '<option class="customSelect"> -- Please Enter a value -- </option>';
+                        foreach($field['choices'] as $choice){
+                            echo '<option class="customSelect" value="'.$choice.'">'.$choice.'</option>';
+                        }
+                    echo '</select>';
                 break;
-
                 default:
-                    echo '<p>This is a the default input</p>';
+                    echo $customValues[$fieldID][0];
+                    echo '<br>';
+                    echo '<label for="'.$fieldID.'">'.$field['title'].'</label>';
+                    echo '<input type="text" name="'.$fieldID.'" class="inputField">';
                 break;
             }
         }
     }
-    // var_dump($fields);
 }
+
+
+function save_custom_metaboxes($postID){
+    global $metaboxes;
+
+    if (! wp_verify_nonce($_POST['post_format_meta_box_nonce'], basename(__FILE__) ) ) {
+        return $postID;
+    }
+
+    if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) {
+        return $postID;
+    }
+
+    if ($_POST['post_type'] == 'page' ) {
+        if (! current_user_can('edit_page', $postID) ) {
+            return $postID;
+        }
+        } elseif (! current_user_can('edit_page', $postID)) {
+            return $postID;
+        }
+
+
+        $postType = get_post_type();
+        foreach ($metaboxes as $metaboxID => $metabox) {
+            if ($metabox['post_type'] == $postType) {
+                $fields = $metabox['fields'];
+                foreach ($fields as $fieldID => $field) {
+                    $oldValue = get_post_meta($postID, $fieldID, true);
+                    $newValue = $_POST[$fieldID];
+
+                    if ($newValue && $newValue != $oldValue) {
+                        update_post_meta($postID, $fieldID, $newValue);
+                    } elseif($newValue == ''|| ! isset($_POST[$fieldID]) ) {
+                        delete_post_meta($postID, $fieldID, $oldValue);
+                    }
+                }
+            }
+        }
+
+
+
+}
+
+add_action('save_post', 'save_custom_metaboxes');
+
